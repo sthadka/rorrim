e2d801e255eb11df7c62069920296973dbdd373a
Sukumar Yethadka <sukumar@thinkapi.com>
2013-04-07 20:16:26 +0200

    Add update and delete repo to scm

--- src/rorrim_scm.erl
+++ src/rorrim_scm.erl
@@ -12,7 +12,7 @@
 
 -include("rorrim.hrl").
 
--export([create/1, status/1, update/1, delete/1]).
+-export([create/1, update/1, delete/1]).
 
 create(Repo) ->
     download_repo(rorrim_conf:get(Repo)).
@@ -21,38 +21,44 @@ update(Repo) ->
     update_repo(rorrim_conf:get(Repo)).
 
 delete(Repo) ->
-    update_repo(rorrim_conf:get(Repo)).
-
-status(Repo) ->
-    update_repo(rorrim_conf:get(Repo)).
+    delete_repo(rorrim_conf:get(Repo)).
 
 
 %% -------------------------------------------------------------------
 %% Helper functions
 %% -------------------------------------------------------------------
 
-download_repo(#repo{scm_type=git, scm_rev=undefined}=RepoDetails) ->
-    download_repo(RepoDetails#repo{scm_rev={branch, "HEAD"}});
-download_repo(#repo{scm_type=git, scm_rev=""}=RepoDetails) ->
-    download_repo(RepoDetails#repo{scm_rev={branch, "HEAD"}});
 download_repo(#repo{scm_type=git, name=Name, scm_url=Url, scm_rev=Rev}) ->
-    RepoDir = repo_dir(Name),
-    ok = filelib:ensure_dir(RepoDir),
+    AppDir = app_dir(Name),
+    ok = filelib:ensure_dir(AppDir),
     rorrim_util:sh(?FMT("git clone -n ~s ~s",
-                        [Url, filename:basename(RepoDir)]),
+                        [Url, filename:basename(AppDir)]),
                    [{cd, ?REPO_DIR}]),
     case Rev of
         {branch, Branch} ->
             rorrim_util:sh(?FMT("git checkout -q origin/~s", [Branch]),
-                           [{cd, RepoDir}]);
+                           [{cd, AppDir}]);
         {tag, Tag} ->
             rorrim_util:sh(?FMT("git checkout -q ~s", [Tag]),
-                           [{cd, RepoDir}])
+                           [{cd, AppDir}])
     end.
 
+update_repo(#repo{scm_type=git, name=Name, scm_rev=Rev}) ->
+    AppDir = app_dir(Name),
+    ok = filelib:ensure_dir(AppDir),
+    rorrim_util:sh("git fetch origin", [{cd, AppDir}]),
+    case Rev of
+        {branch, Branch} ->
+            rorrim_util:sh(?FMT("git checkout -q origin/~s", [Branch]),
+                           [{cd, AppDir}]);
+        {tag, Tag} ->
+            rorrim_util:sh(?FMT("git checkout -q ~s", [Tag]),
+                           [{cd, AppDir}])
+    end.
 
-update_repo(#repo{}=RepoDetails) ->
-    RepoDetails.
+delete_repo(#repo{name=Name}) ->
+    AppDir = app_dir(Name),
+    rorrim_util:sh(?FMT("rm -rf ~s", [AppDir]), []).
 
-repo_dir(RepoName) ->
+app_dir(RepoName) ->
     ?REPO_DIR ++ rorrim_util:get_list(RepoName).
